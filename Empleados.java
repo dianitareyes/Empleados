f27f2ee0022d755e92cac1543dcb16e1be12bf03
Minorusal <minorusal@hotmail.com>
2024-12-10 20:10:36 -0600

    Declaración de HashSet para almacenar los IDs, método ingresarDatos(), bucle para ingresar ID del empleado, comprobración de la validez de ID, verificación de si el ID  ya ha sido registradoif (!idsRegistrados.contains(idEmpleado)), manejo de entradas no validas mostrando un mensaje de error y solicitando uno nuevo o si este es correcto se agrega al conjunto y el bucle termina, igual marca error si igresa un valor numerico y garantiza que no se repita los ids de los empleados

--- Empleados.java
+++ Empleados.java
@@ -6,6 +6,30 @@ public class Empleado {
     private int idEmpleado;
     private String nombreEmpleado;
     private String puesto;
-    private double[] sueldoQuincenal = new double[2]; 
+    private double[] sueldoQuincenal = new double[2];
+
+     private static HashSet<Integer> idsRegistrados = new HashSet<>();
+
+    
+    public void ingresarDatos() {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        while (true) {
+            System.out.print("Ingrese el ID del empleado (único y numérico): ");
+            if (scanner.hasNextInt()) {
+                idEmpleado = scanner.nextInt();
+                if (!idsRegistrados.contains(idEmpleado)) {
+                    idsRegistrados.add(idEmpleado);
+                    break;
+                } else {
+                    System.out.println("Error: El ID ya está registrado.");
+                }
+            } else {
+                System.out.println("Error: Debe ingresar un número válido.");
+                scanner.next(); 
+            }
+        } 
+    }    
 
 }
