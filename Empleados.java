a2882f9c29754e17c222f10c30d25dcb325c68c0
Minorusal <minorusal@hotmail.com>
2024-12-10 19:46:26 -0600

    Importaciones de Scanner y HashSet, define la clase empleado se declaran las variables  privadas que almacenaran la informacion requerida del empleado

---
+++ Empleados.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.HashSet;
+
+public class Empleado {
+    
+    private int idEmpleado;
+    private String nombreEmpleado;
+    private String puesto;
+    private double[] sueldoQuincenal = new double[2]; 
+
+}
